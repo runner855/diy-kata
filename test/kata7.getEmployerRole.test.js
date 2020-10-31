245af738d8f1e23a8bc9e637cddeb5a62cccb110
Dario Milani <runner855@icloud.com>
2020-10-31 17:08:11 +0100

    emplyerrole test finished

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,22 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    const  employees = [
+    {
+      name: "David",
+      role: "Software Engineer"
+    },
+
+     {
+      name: "John",
+      role: "Project Manager"
+    }
+  ];
+
+    
+    expect(getEmployerRole("David", employees)).toEqual('Software Engineer');
+    expect(getEmployerRole("John", employees)).toEqual('Project Manager');
+
+  });
 });
