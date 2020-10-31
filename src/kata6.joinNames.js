d59cc24acee37e5a36a5df44ab9dc4914211f66d
Dario Milani <runner855@icloud.com>
2020-10-31 15:19:49 +0100

    join names completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+return namesObj.map(x => x.name).join(", ").replace(/,(?=[^,]*$)/, ' &');
+
+};
 
 module.exports = joinNames;
+
+/*
+const joinNames = namesObj => {
+let nM = namesObj.map(x => x.name).join(", ");
+return nM.replace(/,(?=[^,]*$)/, ' &');
+*/
+
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,9 @@
-const { joinNames } = require("../src");
+const { 
+  joinNames 
+} = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Omer'}])).toBe('Bart, Lisa & Omer');
+  });
 });
