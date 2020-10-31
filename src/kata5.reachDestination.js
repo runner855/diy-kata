fa67a587746ddef4500bf608655d44d6231c7878
Dario Milani <runner855@icloud.com>
2020-10-31 14:11:22 +0100

    reach destination copleted kata 5

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,18 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+
+let eat = distance / speed;
+let answer =  Math.ceil(eat * 2)/2;
+return "I will be there in " + answer + " hours";
+
+ 
+
+
+};
 
 module.exports = reachDestination;
+
+/*
+function roundHalf(num) {
+    return Math.round(num*2)/2;
+}
+*/
