a7a72422024c51c90c3b25d25f844946b7bac009
Dario Milani <runner855@icloud.com>
2020-10-31 09:13:17 +0100

    all tests passed

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,29 @@
-const fizzBuzz = number => {};
+const fizzBuzz = (number) => {
+    if(number % 3  === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (number % 5 === 0) {
+        return 'Buzz';
+    }else if (number % 3 === 0) {
+        return 'Fizz';
+    } else  {
+        return number;
+    }
+};
 
-module.exports = fizzBuzz;
+    module.exports = fizzBuzz;
+
+    /* testing
+    const fizzBuzz = (number) => {
+        if(number % 3 === 0) {
+            return 'Fizz';
+        } else if (number % 5 === 0) {
+            return 'Buzz';
+        }else if (number % 3 && 5 === 0) {
+            return 'FizzBuzz';
+        } else if(number  % 3 && 5 !== 0) {
+            return number;
+        }
+    };
+    
+        module.exports = fizzBuzz;
+        */ 
