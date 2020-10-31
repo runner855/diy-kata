d2aa95b0256ae6de2505d428e5640fa43e6d7939
Dario Milani <runner855@icloud.com>
2020-10-31 14:10:09 +0100

    reach destination test copleted

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,12 @@
-const { reachDestination } = require("../src");
+const { 
+  reachDestination 
+} = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I will be there in 4.5 hours");
+    expect(reachDestination(67, 12)).toBe("I will be there in 6 hours");
+    expect(reachDestination(100, 15)).toBe("I will be there in 7 hours");
+
+  });
 });
