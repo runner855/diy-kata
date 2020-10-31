fadb279a4079c563bf09ac5c7f2a08bfe08cbd81
Dario Milani <runner855@icloud.com>
2020-10-31 12:21:32 +0100

    humand dog years completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,26 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+
+
+
+let hcd = [];
+
+
+
+if(number === 1) {
+     hcd = [1, 15, 15];
+} else if(number === 2) {
+     hcd =[2, 24, 24];
+     
+} else if(number > 2) {
+     hcd[0]= number;
+     hcd[1] = (number - 2) * 4 + 24;
+    hcd[2] = (number - 2) * 5 + 24;
+
+}
+return hcd;
+
+
+
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,16 @@
-const { humanCatDogYears } = require("../src");
+const {
+     humanCatDogYears 
+    } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("when human years are passed return array of human cat and dog years", () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect(humanCatDogYears(12)).toEqual([12, 64, 74]);
+    
+
+
+    });
+});
+
+
