992f76ae40fc72c3971908e1a4b0fd26318e8958
Dario Milani <runner855@icloud.com>
2020-10-31 17:09:18 +0100

    test passed

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,13 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+
+    let response = employees.filter(employee => employee.name === employeeName);
+    return response[0].role;
+       
+
+  };
+
+    
 
 module.exports = getEmployerRole;
+
+
